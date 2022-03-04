a58c4315f07399c6ae90ad6ddd5c1eaea04fb520
cc <chengcheng0441@gmail.com>
2022-03-04 16:07:29 -0500

    add master

--- app/src/main/java/com/example/flixster/Movie.kt
+++ app/src/main/java/com/example/flixster/Movie.kt
@@ -1,17 +1,23 @@
 package com.example.flixster
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+import kotlinx.parcelize.IgnoredOnParcel
 import org.json.JSONArray
 
+@Parcelize
 data class Movie(
     val movieId: Int,
+    val ratingAverage: Double,
     private val posterPath: String,
     val title: String,
     val overview: String,
 
-) {
+): Parcelable {
+    @IgnoredOnParcel
     val posterImageUrl = "https://image.tmdb.org/t/p/w342/$posterPath"
-    companion object {
 
+    companion object {
         fun fromJsonArray(movieJsonArray: JSONArray): List<Movie> {
             val movies = mutableListOf<Movie>()
             for(i in 0 until movieJsonArray.length()) {
@@ -19,6 +25,7 @@ data class Movie(
                 movies.add(
                     Movie(
                         movieJson.getInt("id"),
+                        movieJson.getDouble("vote_average"),
                         movieJson.getString("poster_path"),
                         movieJson.getString("title"),
                         movieJson.getString("overview")
